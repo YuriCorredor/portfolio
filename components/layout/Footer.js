8841790cb853420123b6b847b0d85df236ecf373
YuriCorredor <YuriCorredor159@gmail.com>
2023-01-14 21:03:09 -0300

    refactor: :hammer: improved sentences

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -27,35 +27,39 @@ const techs = [
 export default function Footer() {
     return (
         <footer className="relative bg-white w-full">
-            <div className="flex flex-col items-center z-[9999] pb-2 sm:pb-2 p-2 sm:p-8 relative bg-white w-full">
+            <div className="flex flex-col items-center z-[9999] pb-2 sm:pb-2 p-2 sm:p-8 relative bg-gray-100 w-full">
                 <div className="w-full justify-center items-center max-w-7xl flex flex-col p-2 sm:p-8">
                     <h1 className="text-3xl font-bold text-center">How this website was built</h1>
                     <div className="flex flex-col flex-wrap max-w-5xl font-semibold p-2 pt-8 pb-8">
-                        <p>You can check github to see the full source code of this website. But here is a quick 
-                            explanation of how things were done. As stated before, my portfolio was inspired by 
-                            Project Hail Mary, a book by Andy Weir, writer of The Martian. After reading the book, I 
-                            made the decision of making a space themed portfolio.
+                        <p>You can find the full source code for this website on my GitHub repository. 
+                            To give you a quick overview of how it was developed, I'll explain some of the 
+                            key technologies and decisions that went into building it. As I mentioned earlier,
+                            my portfolio was inspired by the book "Project Hail Mary" by Andy Weir, 
+                            so I decided to make it space-themed.
                         </p>
-                        <p>For this project, I used these technologies:</p>
+                        <p>For the project, I used the following technologies:</p>
                         <div className="flex flex-wrap flew-col justify-center">
                             {techs.map((tech, index) => <SkillsItem key={index} name={tech.name} Icon={tech.Icon} color={tech.color} />)}
                         </div>
-                        <p>There are other dependencies used on the project like "react-intersection-observer" and 
-                            "react-icons", if you wanna know all of them, check the source code.
+                        <p>
+                            Other dependencies used in the project include "react-intersection-observer" 
+                            and "react-icons". If you're interested in seeing all of the dependencies, 
+                            I encourage you to check out the source code on GitHub.
                         </p>
-                        <p>Next.js was used because I prefer it over plain React, but the project does not need it at
-                            all and it's not even taking advantage of its capabilities. Why TAILWIND? It makes the
-                            development process faster for me and that's the main reason. Three.js was used to render
-                            the stars geometries in the background and the little astronaut that can be seen at the
-                            beggining. I was planning on using more 3D objects on the scene but i really want devices
-                            without a GPU to be able to expirience the website. I plan on making other 3D experiences
-                            more GPU intensive, more visually stunning, but i wanted this portfolio to be accessible.
-                            That's why it lacks 3D objects. That's why the astronaut is low-poly.
+                        <br />
+                        <p>I prefer using Next.js over plain React, but the project doesn't really need it and 
+                            I'm not taking full advantage of its capabilities. Why TAILWIND? I find that using Tailwind 
+                            speeds up my development process and that's the main reason I chose it. Three.js 
+                            was used to render the stars geometries in the background and the little 
+                            astronaut that can be seen at the beginning. I had planned on using more 3D 
+                            objects on the scene, but I wanted to make sure the website would be accessible 
+                            even on devices without a powerful GPU. That's why the astronaut is low-poly and 
+                            there aren't many 3D objects.
                         </p>
                         <p>
-                            Framer motion was used to make the DOM elements animate. The library react-intersection-observer
-                            was used to intercept when an object came into view, so i could use framer motion to animate
-                            it.
+                            Framer Motion was used to animate the DOM elements. The library 
+                            react-intersection-observer was used to detect when an object came into view, 
+                            so I could trigger animations with Framer Motion.
                         </p>
                     </div>
                 </div>
