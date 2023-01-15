6cd5e082346e7fef9be7d75704bf6e0fbc547266
YuriCorredor <YuriCorredor159@gmail.com>
2023-01-14 21:04:48 -0300

    refactor: :hammer: remove unused import

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function index() {
         <title>Yuri Corredor | Developer</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Yuri's portfolio. I solve problems using code (Mostly web). Engineer from Somewhere and everywhere."/>
-        <meta data-react-helmet="true" name="keywords" content="web developer, web designer, software engineer, freelancer, programmer, platform builder, MVP specialist, automation, tech, Essex, London, contractor, consultant, SaaS builder, backend specialist, TypeScript, node.js, AMQP, TCP, HTTP, websockets, flutter, mobile, react, ios, android, AWS, gcloud, mentor, docker, k8s, kubernetes, lambda, functions, PWAs, architect"/>
+        <meta data-react-helmet="true" name="keywords" content="web developer, web designer, software engineer, freelancer, programmer, platform builder, MVP specialist, automation, tech, Essex, London, contractor, consultant, SaaS builder, backend specialist, TypeScript, node.js, AMQP, TCP, HTTP, websockets, flutter, mobile, react, ios, android, AWS, architect"/>
     </Head>
     <Main />
     </>
