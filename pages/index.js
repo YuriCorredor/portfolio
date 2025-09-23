67db52d64fd38a6630002bc4f73c2381a424c078
Yuri Corredor <91801065+YuriCorredor@users.noreply.github.com>
2025-09-22 23:04:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import Main from "../components/Main"
 import Head from "next/head"
+import Script from "next/script"
 
-export default function index() {
-    return <>
-    <Head>
+export default function Index() {
+  return (
+    <>
+      <Head>
         <title>Yuri Corredor | Developer</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Yuri's portfolio. I solve problems using code (Mostly web). Engineer from Somewhere and everywhere."/>
-        <meta data-react-helmet="true" name="keywords" content="web developer, web designer, software engineer, freelancer, programmer, platform builder, MVP specialist, automation, tech, Essex, London, contractor, consultant, SaaS builder, backend specialist, TypeScript, node.js, AMQP, TCP, HTTP, websockets, flutter, mobile, react, ios, android, AWS, architect"/>
-    </Head>
-    <Main />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+        />
+        <meta
+          name="description"
+          content="Yuri's portfolio. I solve problems using code (Mostly web). Engineer from Somewhere and everywhere."
+        />
+        <meta
+          data-react-helmet="true"
+          name="keywords"
+          content="web developer, web designer, software engineer, freelancer, programmer, platform builder, MVP specialist, automation, tech, Essex, London, contractor, consultant, SaaS builder, backend specialist, TypeScript, node.js, AMQP, TCP, HTTP, websockets, flutter, mobile, react, ios, android, AWS, architect"
+        />
+      </Head>
+
+      {/* Google AdSense Script */}
+      <Script
+        id="adsense-script"
+        async
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4182724923768639"
+      />
+
+      <Main />
     </>
+  )
 }
